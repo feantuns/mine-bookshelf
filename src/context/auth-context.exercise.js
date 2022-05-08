204d2959fec5b1514918d19c1e676b2d2d04975d
Felipe Antunes <felipeantunes@MacBook-Air-de-Felipe.local>
2022-05-08 14:16:46 -0300

    create useAuth hook

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -2,3 +2,11 @@ import React from 'react'
 // 🐨 create and export a React context variable for the AuthContext
 // 💰 using React.createContext
 export const AuthContext = React.createContext()
+
+export const useAuth = () => {
+  const auth = React.useContext(AuthContext)
+  if (!auth) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return auth
+}
--- src/utils/books.exercise.js
+++ src/utils/books.exercise.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useQuery, queryCache} from 'react-query'
-import {AuthContext} from 'context/auth-context'
+import {useAuth} from 'context/auth-context'
 import {client} from './api-client'
 import bookPlaceholderSvg from 'assets/book-placeholder.svg'
 
@@ -36,13 +36,13 @@ const getBookSearchConfig = (query, user) => ({
 })
 
 function useBookSearch(query) {
-  const {user} = React.useContext(AuthContext)
+  const {user} = useAuth()
   const result = useQuery(getBookSearchConfig(query, user))
   return {...result, books: result.data ?? loadingBooks}
 }
 
 function useBook(bookId) {
-  const {user} = React.useContext(AuthContext)
+  const {user} = useAuth()
   const {data} = useQuery({
     queryKey: ['book', {bookId}],
     queryFn: () =>
@@ -60,7 +60,7 @@ function useBook(bookId) {
 // refetchBookSearchQuery function. It should no longer need to accept user as
 // an argument and instead lists it as a dependency.
 function useRefetchBookSearchQuery() {
-  const {user} = React.useContext(AuthContext)
+  const {user} = useAuth()
   return React.useCallback(async () => {
     queryCache.removeQueries('bookSearch')
     await queryCache.prefetchQuery(getBookSearchConfig('', user))
